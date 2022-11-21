77c3fef4e7c59325ad3af3dec7e10752410311bd
tomaruz <tomaruz2002@gmail.com>
2022-11-21 23:40:31 +0000

    updated

---
+++ js/storytelling.js
@@ -0,0 +1,7 @@
+console.log("hello")
+
+// when scroll, elements animate
+// window.addEventListener('scroll', () => {
+// document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+// }, false);
+
